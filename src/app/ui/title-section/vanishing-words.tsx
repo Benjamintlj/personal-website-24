58b91c070018d092ea8e5e197ae6995d63725ec6
n0928518 <n0928518@my.ntu.ac.uk>
2024-07-17 12:50:36 +0100

    dublicate bug vanishing words

--- src/app/ui/title-section/vanishing-words.tsx
+++ src/app/ui/title-section/vanishing-words.tsx
@@ -45,7 +45,7 @@ export const VanishingWords = ({
             setCurrWordSize(currDiv.current.offsetWidth)
             setXOffSet(currWordSize / 2 - prevWordSize / 2)
         }
-    }, [prevWord, currWord])
+    }, [currWord])
 
     useEffect(() => {
         if (!isAnimating) {
@@ -54,67 +54,94 @@ export const VanishingWords = ({
             }, duration)
             return () => clearTimeout(timeout)
         }
-    }, [isAnimating, startAnimation, duration])
+    }, [isAnimating])
 
     return (
-        <div
-            className={`${className} relative w-full flex items-center justify-center`}
+        <AnimatePresence
+            onExitComplete={() => {
+                console.log('exit')
+                setIsAnimating(false)
+            }}
         >
-            <AnimatePresence
-                onExitComplete={() => {
-                    console.log('exit')
-                    setIsAnimating(false)
+            <motion.div
+                className={`${className}`}
+                key={`${currentWord}`}
+                ref={divRef}
+                initial={{
+                    x: xOffSet,
                 }}
+                transition={{
+                    duration: 1,
+                    ease: 'easeInOut',
+                }}
+                animate={{ x: 0 }}
             >
+                <p className="inline-block">{relativeClause}</p>
+                <p className={`inline-block w-1`}></p>
+
                 <motion.div
-                    key={`${currentWord}-${xOffSet}`}
-                    ref={divRef}
-                    transition={{ duration: 1 }}
-                    initial={{ x: xOffSet }}
-                    animate={{ x: 0 }}
+                    initial={{
+                        opacity: 0,
+                        y: 10,
+                    }}
+                    animate={{
+                        opacity: 1,
+                        y: 0,
+                    }}
+                    transition={{
+                        duration: 0.4,
+                        ease: 'easeInOut',
+                        type: 'spring',
+                        stiffness: 100,
+                        damping: 10,
+                    }}
+                    exit={{
+                        opacity: 0,
+                        y: -40,
+                        x: 40,
+                        filter: 'blur(8px)',
+                        scale: 2,
+                        position: 'absolute',
+                    }}
+                    className={cn(
+                        'z-10 inline-block relative text-left text-neutral-900 dark:text-neutral-100 px-2',
+                        className
+                    )}
                 >
-                    <p className="inline-block">{relativeClause}</p>
-                    <motion.div
-                        key={currentWord}
-                        className={cn(
-                            'z-10 inline-block relative text-left text-neutral-900 dark:text-neutral-100 px-2',
-                            className
-                        )}
-                    >
-                        {currentWord.split('').map((letter, index) => (
-                            <motion.span
-                                key={currentWord + index}
-                                initial={{
-                                    opacity: 0,
-                                    y: 10,
-                                    filter: 'blur(8px)',
-                                }}
-                                animate={{
-                                    opacity: 1,
-                                    y: 0,
-                                    filter: 'blur(0px)',
-                                }}
-                                transition={{
-                                    delay: index * 0.08,
-                                    duration: 0.3,
-                                }}
-                                className={clsx(
-                                    'inline-block',
-                                    letter === ' ' && 'w-1'
-                                )}
-                            >
-                                {letter}
-                            </motion.span>
-                        ))}
-                    </motion.div>
+                    {currentWord.split('').map((letter, index) => (
+                        <motion.span
+                            key={currentWord + index}
+                            initial={{
+                                opacity: 0,
+                                y: 10,
+                                filter: 'blur(8px)',
+                            }}
+                            animate={{
+                                opacity: 1,
+                                y: 0,
+                                filter: 'blur(0px)',
+                            }}
+                            transition={{
+                                delay: index * 0.08,
+                                duration: 0.3,
+                            }}
+                            className={clsx(
+                                'inline-block z-10 relative text-left text-neutral-900 dark:text-neutral-100',
+                                letter === ' ' && 'w-1'
+                            )}
+                        >
+                            {letter}
+                        </motion.span>
+                    ))}
                 </motion.div>
-            </AnimatePresence>
+            </motion.div>
 
             <div
                 style={{
                     position: 'absolute',
                     left: '-10000px',
                 }}
+                className={`${className}`}
             >
                 <div
                     style={{
@@ -148,6 +175,82 @@ export const VanishingWords = ({
                     </div>
                 </div>
             </div>
-        </div>
+        </AnimatePresence>
     )
 }
+
+/**
+ * export const VanishingWords = ({
+ *     words,
+ *     duration = 3000,
+ *     relativeClause,
+ *     className,
+ * }: {
+ *     words: string[]
+ *     duration?: number
+ *     relativeClause: string
+ *     className?: string
+ * }) => {
+ *     const [currentWord, setCurrentWord] = useState(words[0])
+ *     const [isAnimating, setIsAnimating] = useState<boolean>(false)
+ *
+ *     const startAnimation = useCallback(() => {
+ *         const word = words[words.indexOf(currentWord) + 1] || words[0]
+ *         setCurrentWord(word)
+ *
+ *         setIsAnimating(true)
+ *     }, [currentWord, words])
+ *
+ *     useEffect(() => {
+ *         if (!isAnimating) {
+ *             const timeout = setTimeout(() => {
+ *                 startAnimation()
+ *             }, duration)
+ *             return () => clearTimeout(timeout)
+ *         }
+ *     }, [isAnimating, startAnimation, duration])
+ *
+ *     return (
+ *         <AnimatePresence
+ *             onExitComplete={() => {
+ *                 console.log('exit')
+ *                 setIsAnimating(false)
+ *             }}
+ *         >
+ *             <motion.div
+ *                 key={`${currentWord}-1`}
+ *                 className={cn(
+ *                     'z-10 inline-block relative text-left text-neutral-900 dark:text-neutral-100 px-2'
+ *                 )}
+ *             >
+ *                 <p className="inline-block">{relativeClause}</p>
+ *                 {currentWord.split('').map((letter, index) => (
+ *                     <motion.span
+ *                         key={currentWord + index}
+ *                         initial={{
+ *                             opacity: 0,
+ *                             y: 10,
+ *                             filter: 'blur(8px)',
+ *                         }}
+ *                         animate={{
+ *                             opacity: 1,
+ *                             y: 0,
+ *                             filter: 'blur(0px)',
+ *                         }}
+ *                         transition={{
+ *                             delay: index * 0.08,
+ *                             duration: 0.3,
+ *                         }}
+ *                         className={clsx(
+ *                             'inline-block',
+ *                             letter === ' ' && 'w-1'
+ *                         )}
+ *                     >
+ *                         {letter}
+ *                     </motion.span>
+ *                 ))}
+ *             </motion.div>
+ *         </AnimatePresence>
+ *     )
+ * }
+ */
