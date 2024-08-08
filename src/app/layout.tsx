49d30a2c54cf6194aa1758aeb62ddf176b614830
n0928518 <n0928518@my.ntu.ac.uk>
2024-08-08 12:03:36 +0100

    metadata for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,22 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
     title: 'Ben Lewis-Jones',
-    description:
-        'Personal website, showing off who Ben is and what he is all about.',
-    metadataBase: new URL('https://www.benlewisjones.com'),
+    metadataBase: new URL('https://benlewisjones.com'),
+    keywords: [
+        'Programming',
+        'Computer Science',
+        'Ben Lewis-Jones',
+        'Ben Lewis Jones',
+        'Personal website',
+        'portfolio',
+        'Software engineer',
+        'Software developer',
+    ],
+    openGraph: {
+        description:
+            "Personal website, showing off Ben's professional and personal life.",
+        images: ['/images/meta/selfie.png'],
+    },
 }
 
 export default function RootLayout({
